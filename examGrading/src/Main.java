a7a3433c5cbe96bbb4f9439ac1f41268b22c545c
Nugraha Varrel Kusuma <72533@fif.co.id>
2025-09-04 09:51:26 +0700

    Create input for user Nilai

--- examGrading/src/Main.java
+++ examGrading/src/Main.java
@@ -1,19 +1,33 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
+        Scanner input = new Scanner(System.in);
+        int totalSiswa = 0;
+        double totalNilaiSiswa = 0;
 
-        int [] nilaiSiswa = {85,70,60,90,65};
-        int totalNilaiSiswa = 0;
-        for (int i :nilaiSiswa) {
-            totalNilaiSiswa += i;
+        while (true) {
+            System.out.println("Masukkan nilai siswa : ");
+            while (!input.hasNextInt()){
+                System.out.println("Input harus berupa angka!");
+                input.next();
+            }
+            int tempNilai = input.nextInt();
+
+            if (tempNilai < 0){
+                break;
+            }
+
+            totalNilaiSiswa += tempNilai;
+            totalSiswa++;
+
         }
 
-        double rataRataNilaiSiswa = totalNilaiSiswa / nilaiSiswa.length;
-        System.out.println(+totalNilaiSiswa);
-        System.out.println(+rataRataNilaiSiswa);
+        double rataRataNilaiSiswa = totalNilaiSiswa / totalSiswa;
 
         char grade;
         if (rataRataNilaiSiswa >= 85){
@@ -26,7 +40,8 @@ public class Main {
             grade = 'D';
         }
 
+        System.out.println("Total Nilai Siswa     : "+totalNilaiSiswa);
         System.out.println("Rata-rata nilai Siswa : "+rataRataNilaiSiswa);
-        System.out.println("Grade : "+grade);
+        System.out.println("Grade                 : "+grade);
     }
 }
